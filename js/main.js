b5a41512c4e4064db2b86448e45844f65d10a463
Anna Lukianova <ann.lookyanova@gmail.com>
2019-09-06 18:43:10 +0200

    Fix slideshow bug

--- js/main.js
+++ js/main.js
@@ -3,41 +3,37 @@ function show(n) {
   document.querySelector('.property-' + n).classList.add('property-visible')
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+document.querySelectorAll('.slideshow').forEach(initSlideshow)
 
-document.querySelectorAll('.slideshow__prev').forEach(
-  function (button) {
-    button.addEventListener('click', function () {
+function initSlideshow(slideshow) {
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  slideshow.querySelector('.slideshow__prev')
+    .addEventListener('click', function () {
       plusSlides(-1)
     })
-  }
-)
 
-document.querySelectorAll('.slideshow__next').forEach(
-  function (button) {
-    button.addEventListener('click', function () {
+  slideshow.querySelector('.slideshow__next')
+    .addEventListener('click', function () {
       plusSlides(1)
     })
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
-)
 
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slideshow__slide");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function showSlides(n) {
+    let i;
+    let slides = slideshow.querySelectorAll('.slideshow__slide');
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
   }
-  slides[slideIndex - 1].style.display = "block";
 }
+
+
+
