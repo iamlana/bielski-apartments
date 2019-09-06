4588dfb30a3144c541a831033564f14e438ec78c
Anna Lukianova <ann.lookyanova@gmail.com>
2019-09-06 18:53:08 +0200

    Refactor slider code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,33 @@
 function show(n) {
-  document.querySelector('.property-visible').classList.remove('property-visible')
-  document.querySelector('.property-' + n).classList.add('property-visible')
+  document.querySelector('.property-visible')
+    .classList.remove('property-visible');
+  document.querySelector('.property-' + n)
+    .classList.add('property-visible');
 }
 
-document.querySelectorAll('.slideshow').forEach(initSlideshow)
+document.querySelectorAll('.slideshow').forEach(initSlideshow);
 
 function initSlideshow(slideshow) {
-  let slideIndex = 1;
-  showSlides(slideIndex);
+  const slides = slideshow.querySelectorAll('.slideshow__slide');
+  let currentSlide = 0;
+  showSlides();
 
   slideshow.querySelector('.slideshow__prev')
-    .addEventListener('click', function () {
-      plusSlides(-1)
-    })
+    .addEventListener('click', () => changeSlide(-1));
 
   slideshow.querySelector('.slideshow__next')
-    .addEventListener('click', function () {
-      plusSlides(1)
-    })
+    .addEventListener('click', () => changeSlide(+1));
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  function changeSlide(count) {
+    currentSlide = (slides.length + currentSlide + count) % slides.length;
+    showSlides();
   }
 
-  function showSlides(n) {
-    let i;
-    let slides = slideshow.querySelectorAll('.slideshow__slide');
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
+  function showSlides() {
+    for (const slide of slides) {
+      slide.style.display = 'none';
     }
-    slides[slideIndex - 1].style.display = 'block';
+    slides[currentSlide].style.display = 'block';
   }
 }
 
