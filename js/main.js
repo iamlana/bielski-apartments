268fc0ec2d3c2b97e0c457b033eed6fbe91b396e
Anna Lukianova <ann.lookyanova@gmail.com>
2019-10-28 18:52:50 +0100

    Refactor rooms-selection

--- js/main.js
+++
@@ -1,6 +0,0 @@
-function show(n) {
-  document.querySelector('.property-visible')
-    .classList.remove('property-visible');
-  document.querySelector('.property-' + n)
-    .classList.add('property-visible');
-}
