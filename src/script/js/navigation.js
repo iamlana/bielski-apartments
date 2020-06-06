49e924a72966ffc747771ad3c62fe04c02118611
Anna Lukianova <ann.lookyanova@gmail.com>
2020-06-06 18:49:22 +0200

    Change navigation on scroll

--- src/script/js/navigation.js
+++ src/script/js/navigation.js
@@ -36,3 +36,13 @@ function setupNavigation (navigation) {
     link.addEventListener('click', close);
   });
 }
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
