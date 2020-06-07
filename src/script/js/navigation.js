7b82c4b2ac8f978bf574f5fb2315b4c46ffb5b0b
Anna Lukianova <ann.lookyanova@gmail.com>
2020-06-07 17:31:56 +0200

    Fix navbar

--- src/script/js/navigation.js
+++ src/script/js/navigation.js
@@ -42,7 +42,7 @@ let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
 }
